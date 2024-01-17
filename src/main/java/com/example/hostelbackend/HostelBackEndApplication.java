a9a527dd21b932d9e3f951a436fff6c3dc7cd1e7
FarAlex <sibacadem@mail.ru>
2024-01-17 21:45:26 +0700

    Старт

---
+++ src/main/java/com/example/hostelbackend/HostelBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.hostelbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostelBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HostelBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hostelbackend/HostelBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hostelbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostelBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
